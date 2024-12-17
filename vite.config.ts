aaa71c0363b09736efb8e3ad4acb14cd8b2c19f5
aoqlsdl <hyeeev@gmail.com>
2024-12-17 20:11:48 +0900

    chore: 프로젝트 세팅

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'url';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
+	build: {
+		outDir: 'dist',
+		rollupOptions: {
+			input: {
+				main: fileURLToPath(new URL('./index.html', import.meta.url)),
+			},
+		},
+	},
+});
